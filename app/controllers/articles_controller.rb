019bbf92d9168d3ed294e9447d6714ea71d26949
wamarcum <allan_marcum@outlook.com>
2014-11-18 17:52:40 -0600

    Add deletion functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,11 @@ include ArticlesHelper
 		@article.save
 		redirect_to article_path(@article)
 	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
 end
